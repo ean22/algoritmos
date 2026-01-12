4cbacbbcaa3f122192cca599b27f76af4cca78e5
Eduardo Alves Nunes <eduardo.nunes16@fatec.sp.gov.br>
2026-01-12 15:16:24 +0000

    .

---
+++ src/romanToInteger/App.java
@@ -0,0 +1,45 @@
+package romanToInteger;
+
+import java.util.Map;
+import java.util.Map;
+
+public class App {
+    public static void main(String[] args){
+        String num = "XLIX";
+
+        System.out.println("""
+                %s = %d
+                """.formatted(num, Solution.romanToInteger(num)));
+    }
+}
+
+class Solution{
+    final static Map<Character, Integer> map = Map.of(
+       'I',1,
+       'V',5,
+       'X',10,
+       'L',50,
+       'C',100,
+       'D',500,
+       'M',1000
+    );
+
+    public static int romanToInteger(String num){
+        int sum = 0;
+        int i;
+
+        for(i = 0; i < num.length() - 1; i++){
+            
+            int actual = map.get(num.charAt(i));
+            int next = map.get(num.charAt(i + 1));
+            
+            if(actual >= next){
+                sum += actual;
+            } else sum -= actual;
+        }
+
+        sum += map.get(num.charAt(i));
+
+        return sum;
+    }
+}
