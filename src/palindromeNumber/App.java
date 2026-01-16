9d0b25ab90588b83487f0db825ab26f51844e4a6
Eduardo Alves Nunes <eduardo.nunes16@fatec.sp.gov.br>
2026-01-16 22:13:06 +0000

    .

--- src/palindromeNumber/App.java
+++ src/palindromeNumber/App.java
@@ -2,34 +2,57 @@ package palindromeNumber;
 
 public class App {
     public static void main(String[] args){
-        int num = 11211;
+        int num = 112111;
+
+        Solution s = new Solution();
 
         System.out.println("""
                 Número %d é Palindrome?:
                 %b
-                """.formatted(num, Solution.isPalindrome(num)));
+                """.formatted(num, s.isPalindrome(num)));
     }
 }
 
-class Solution{
-    public static boolean isPalindrome(int num){
+// class Solution{
+//     public static boolean isPalindrome(int num){
         
-        if(num < 0) return false;
+//         if(num < 0) return false;
 
-        return checkPalindrome(String.valueOf(num));
-    }
+//         return checkPalindrome(String.valueOf(num));
+//     }
 
-    private static boolean checkPalindrome(String num){
-        int left = 0;
-        int right = num.length() - 1;
+//     private static boolean checkPalindrome(String num){
+//         int left = 0;
+//         int right = num.length() - 1;
 
-        while(left < right){
-            if(num.charAt(left) != num.charAt(right)) return false;
+//         while(left < right){
+//             if(num.charAt(left) != num.charAt(right)) return false;
 
-            left++;
+//             left++;
+//             right--;
+//         }
+
+//         return true;
+//     }
+// }
+
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        char[] charArr = String
+                .valueOf(x)
+                .toCharArray();
+        int left, right;
+        left = 0;
+        right = charArr.length - 1;
+
+        for (left = 0; left <= right; left++) {
+            if(charArr[left] != charArr[right]){
+                return false;
+            }
+
             right--;
         }
-
         return true;
     }
 }
