1271b36a31f25199e70458ff551ce04bac02a6ce
Eduardo Alves Nunes <eduardo.nunes16@fatec.sp.gov.br>
2026-01-17 00:40:53 +0000

    .

--- src/romanToInteger/App.java
+++ src/romanToInteger/App.java
@@ -9,37 +9,47 @@ public class App {
 
         System.out.println("""
                 %s = %d
-                """.formatted(num, Solution.romanToInteger(num)));
+                """.formatted(num, Solution.romanToInt(num)));
     }
 }
 
-class Solution{
-    final static Map<Character, Integer> map = Map.of(
-       'I',1,
-       'V',5,
-       'X',10,
-       'L',50,
-       'C',100,
-       'D',500,
-       'M',1000
-    );
+// class Solution{
+//     final static Map<Character, Integer> map = Map.of(
+//        'I',1,
+//        'V',5,
+//        'X',10,
+//        'L',50,
+//        'C',100,
+//        'D',500,
+//        'M',1000
+//     );
 
-    public static int romanToInteger(String num){
-        int sum = 0;
-        int i;
+//     public static int romanToInt(String num){
+//         int sum = 0;
+//         int i;
 
-        for(i = 0; i < num.length() - 1; i++){
+//         for(i = 0; i < num.length() - 1; i++){
             
-            int actual = map.get(num.charAt(i));
-            int next = map.get(num.charAt(i + 1));
+//             int actual = map.get(num.charAt(i));
+//             int next = map.get(num.charAt(i + 1));
             
-            if(actual >= next){
-                sum += actual;
-            } else sum -= actual;
-        }
+//             if(actual >= next){
+//                 sum += actual;
+//             } else sum -= actual;
+//         }
 
-        sum += map.get(num.charAt(i));
+//         sum += map.get(num.charAt(i));
 
-        return sum;
+//         return sum;
+//     }
+// }
+
+
+class Solution {
+    public int romanToInt(String s) {
+        
+
+        
+        return 0;
     }
 }
