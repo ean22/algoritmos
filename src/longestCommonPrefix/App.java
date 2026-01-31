96f4094d2b69ba20158e3d6f9f904970e7c04bf3
Eduardo Alves Nunes <eduardo.nunes16@fatec.sp.gov.br>
2026-01-31 17:42:31 +0000

    (╥﹏╥)

--- src/longestCommonPrefix/App.java
+++ src/longestCommonPrefix/App.java
@@ -2,7 +2,7 @@ package longestCommonPrefix;
 
 public class App {
     public static void main(String[] args){
-        String[] str = new String[] {"dog","racecar","car"};
+        String[] str = new String[] {"dog","dracecar","dcar"};
 
         Solution s = new Solution();
 
@@ -10,29 +10,63 @@ public class App {
     }
 }
 
+// class Solution {
+//     public String longestCommonPrefix(String[] strs) {
+//         String str = "";
+//         int minor = Integer.MAX_VALUE;
+//         int letra = 0;
+//         int palavra = 0;
+
+//         for (String word : strs) {
+//             if(word.length() < minor) minor = word.length();
+//         }
+
+//         for (letra = 0; letra < minor; letra++) {
+            
+//             char c = strs[0].charAt(letra);
+
+//             for (palavra = 0; palavra < strs.length; palavra++) {
+//                 if(strs[palavra].charAt(letra) != c) return str;
+
+//             }
+//             str = str.concat(String.valueOf(c));
+//         }
+  
+//         return str;
+//     }
+// }
+
+
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        String str = "";
-        int minor = Integer.MAX_VALUE;
-        int letra = 0;
-        int palavra = 0;
+        int letter = 0;
+        int word = 0;
+        int lastChar = smallerWord(strs);
+        String prefix = "";
 
-        for (String word : strs) {
-            if(word.length() < minor) minor = word.length();
-        }
+        for (letter = 0; letter < lastChar; letter++) {
+            char c = strs[0].charAt(letter);
 
-        for (letra = 0; letra < minor; letra++) {
-            
-            char c = strs[0].charAt(letra);
-
-            for (palavra = 0; palavra < strs.length; palavra++) {
-                if(strs[palavra].charAt(letra) != c) return str;
-
+            for (word = 1; word < strs.length; word++) {
+                if (c != strs[word].charAt(letter)) return prefix;
             }
-            str = str.concat(String.valueOf(c));
+            prefix = prefix.concat(String.valueOf(c));
         }
-  
-        return str;
+
+        return prefix;
+    }
+
+    private int smallerWord(String[] strs){
+        int smaller = Integer.MAX_VALUE;
+
+        for (String string : strs) {
+            int stringSize = string.length();
+
+            if (stringSize < smaller) smaller = stringSize;
+        }
+
+        return smaller;
+
     }
 }
 
---
+++ src/mergeTwoSortedLists/App.java
@@ -0,0 +1,66 @@
+package mergeTwoSortedLists;
+
+public class App {
+    public static void main( String[] a){
+        ListNode ln1 = new ListNode(1, new ListNode(2, new ListNode(6, new ListNode(8))));
+        ListNode ln2 = new ListNode(1, new ListNode(3, new ListNode(4, new ListNode(5))));
+    
+        Solution s = new Solution();
+        
+        ln1 = s.mergeTwoLists(ln1, ln2);
+        
+        while( ln1 != null){
+            System.out.println( ln1.val );
+            
+            ln1 = ln1.next;
+
+        }
+    }
+
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {};
+    ListNode(int val) { this.val = val; };
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; };
+ }
+ 
+// class Solution {
+
+//     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+//         ListNode head = new ListNode(); 
+//         ListNode currentNode = head;
+        
+
+//         while ( list1 != null && list2 != null ){
+//             if(list1.val < list2.val){
+//                 currentNode.next = list1;
+//                 list1 = list1.next;
+
+//             } else {
+//                 currentNode.next = list2;
+//                 list2 = list2.next;
+                
+//             }
+
+//             currentNode = currentNode.next;
+//         }
+
+//         if( list1 != null) {
+//             currentNode.next = list1;
+
+//         } else currentNode.next = list2;
+
+//         return head.next;
+//     }
+// }
+
+class Solution {
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+  
+    }
+}
