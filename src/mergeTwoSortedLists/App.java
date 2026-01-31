be48ffac6b673fb5e8e2d602c16ba349421f8fb7
Eduardo Alves Nunes <eduardo.nunes16@fatec.sp.gov.br>
2026-01-31 17:56:25 +0000

    (ᵕ—ᴗ—)

--- src/mergeTwoSortedLists/App.java
+++ src/mergeTwoSortedLists/App.java
@@ -61,6 +61,24 @@ class ListNode {
 class Solution {
 
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-  
+        ListNode head = new ListNode(), current = head;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                current.next = list1;
+                current = current.next;
+
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                current = current.next;
+
+                list2 = list2.next;
+            }
+        }
+
+        current.next = (list1 != null) ? list1 : list2;
+
+        return head.next;
     }
 }
