090a516138df1eb29381f9daa9fa2139d7be5bc3
Eduardo Alves Nunes <eduardo.nunes16@fatec.sp.gov.br>
2026-01-31 19:06:44 +0000

    (ᵕ—ᴗ—)

--- src/validParentheses/Solution.java
+++ src/validParentheses/Solution.java
@@ -12,55 +12,29 @@ class App {
     }
 }
 
-// class Solution {
-//     public boolean isValid( String s ) {
-//         Stack<Character> stack = new Stack<>();
-
-//         for ( char c: s.toCharArray() ) {
-//             if ( c == '(' || c == '{' || c == '[' ){
-//                 stack.add(c);
-
-//             } else if( !stack.isEmpty() && c == ')' && stack.peek() == '(' ) {
-//                 stack.pop();
-            
-//             } else if ( !stack.isEmpty() && c == '}' && stack.peek() == '{' ) {
-//                 stack.pop();
-
-//             } else if ( !stack.isEmpty() && c == ']' && stack.peek() == '[' ) {
-//                 stack.pop();
-
-//             } else return false;
-//         }
-
-//         return stack.isEmpty();
-        
-//     }
-// }
-
-
-public class Solution {
-    public boolean isValid(String s) {
+class Solution {
+    public boolean isValid( String s ) {
         Stack<Character> stack = new Stack<>();
 
-        for (int i = 0; i < s.length(); i++){
-            char c = s.charAt(i);
-
-            if (c == '(' || c == '[' || c == '{') {
+        for ( char c: s.toCharArray() ) {
+            if ( c == '(' || c == '{' || c == '[' ){
                 stack.add(c);
 
-            } else if (!stack.isEmpty() && c == ')' && stack.peek() == '(') {
+            } else if( !stack.isEmpty() && c == ')' && stack.peek() == '(' ) {
+                stack.pop();
+            
+            } else if ( !stack.isEmpty() && c == '}' && stack.peek() == '{' ) {
                 stack.pop();
 
-            } else if (!stack.isEmpty() && c == ']' && stack.peek() == '[') {
-                stack.pop();
-
-            } else if (!stack.isEmpty() && c == '}' && stack.peek() == '{') {
+            } else if ( !stack.isEmpty() && c == ']' && stack.peek() == '[' ) {
                 stack.pop();
 
             } else return false;
         }
 
         return stack.isEmpty();
+        
     }
 }
+
 
