f6fef597cf18db4e5a6fe71f0141fd0a88be4082
Eduardo Alves Nunes <eduardo.nunes16@fatec.sp.gov.br>
2026-01-31 17:45:34 +0000

    (╥﹏╥)

--- src/longestCommonPrefix/App.java
+++ src/longestCommonPrefix/App.java
@@ -10,66 +10,31 @@ public class App {
     }
 }
 
-// class Solution {
-//     public String longestCommonPrefix(String[] strs) {
-//         String str = "";
-//         int minor = Integer.MAX_VALUE;
-//         int letra = 0;
-//         int palavra = 0;
-
-//         for (String word : strs) {
-//             if(word.length() < minor) minor = word.length();
-//         }
-
-//         for (letra = 0; letra < minor; letra++) {
-            
-//             char c = strs[0].charAt(letra);
-
-//             for (palavra = 0; palavra < strs.length; palavra++) {
-//                 if(strs[palavra].charAt(letra) != c) return str;
-
-//             }
-//             str = str.concat(String.valueOf(c));
-//         }
-  
-//         return str;
-//     }
-// }
-
-
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        int letter = 0;
-        int word = 0;
-        int lastChar = smallerWord(strs);
-        String prefix = "";
+        String str = "";
+        int minor = Integer.MAX_VALUE;
+        int letra = 0;
+        int palavra = 0;
 
-        for (letter = 0; letter < lastChar; letter++) {
-            char c = strs[0].charAt(letter);
+        for (String word : strs) {
+            if(word.length() < minor) minor = word.length();
+        }
 
-            for (word = 1; word < strs.length; word++) {
-                if (c != strs[word].charAt(letter)) return prefix;
+        for (letra = 0; letra < minor; letra++) {
+            
+            char c = strs[0].charAt(letra);
+
+            for (palavra = 0; palavra < strs.length; palavra++) {
+                if(strs[palavra].charAt(letra) != c) return str;
+
             }
-            prefix = prefix.concat(String.valueOf(c));
+            str = str.concat(String.valueOf(c));
         }
-
-        return prefix;
-    }
-
-    private int smallerWord(String[] strs){
-        int smaller = Integer.MAX_VALUE;
-
-        for (String string : strs) {
-            int stringSize = string.length();
-
-            if (stringSize < smaller) smaller = stringSize;
-        }
-
-        return smaller;
-
+  
+        return str;
     }
 }
-
 /*
 
 flower
