c9df184293bde1b035687306a58aca4d5ab33b99
Eduardo Alves Nunes <eduardo.nunes16@fatec.sp.gov.br>
2026-02-01 15:32:48 +0000

    (ᵕ—ᴗ—)

---
+++ src/removeDuplicates/App.java
@@ -0,0 +1,47 @@
+package removeDuplicates;
+
+public class App {
+    public static void main(String[] args) {
+        int[] arr = {-3,-1,0,0,0,3,3};
+        
+        System.out.print("Array original: ");
+        for (int i : arr) {
+            System.out.print(i + ", ");
+        }
+
+        Solution s = new Solution();
+        System.out.println("\nQuantidade de números únicos: " + s.removeDuplicates(arr));
+        
+        System.out.print("Array alterado: ");
+        for (int i : arr) {
+            System.out.print(i + ", ");
+        }
+
+    }
+}
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int uniqueNum = nums.length;
+        int current = nums[0];
+        int indexA = 0, indexB = 1;
+
+
+        for (int i : nums) {
+            if (i == current) uniqueNum--;
+            current = i;
+        }
+
+        // Ignora o decremento do indice 0;
+        uniqueNum++;
+
+        while (indexB < nums.length) {
+            if (nums[indexA] != nums[indexB]) {
+                nums[++indexA] = nums[indexB++];
+
+            } else indexB++;
+        }
+
+        return uniqueNum;
+    }
+}
